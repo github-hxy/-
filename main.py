ee7687caa786f66787cef60d9f06b38ff6c00228
github-hxy <82249359+github-hxy@users.noreply.github.com>
2022-11-20 00:18:57 +0800

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,8 @@ import os
  
 def get_color():
     # 获取随机颜色
-    get_colors = lambda n: list(map(lambda i: "#" + "%06x" % random.randint(0, 0xFFFFFF), range(n)))
+    # get_colors = lambda n: list(map(lambda i: "#" + "%06x" % random.randint(0, 0xFFFFFF), range(n)))
+    get_colors = pink
     color_list = get_colors(100)
     return random.choice(color_list)
  
